1f23e6ff9326d51be9eae325121f910b1fec1fd6
ZhenyaAK <ekolessova@gmail.com>
2018-04-09 16:00:29 -0400

    organiser.events and event.organiser

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :organiser
-  has_many :attendees, through :event_attendees
+  has_many :attendees, through: :event_attendees
+
+  def organiser
+    Organiser.where(id: organiser_id)
+  end
 end
--- app/models/organiser.rb
+++ app/models/organiser.rb
@@ -4,4 +4,9 @@ class Organiser < ApplicationRecord
   has_secure_password
   validates :name, presence: true
   validates :email_address, presence: true, uniqueness: true
+
+ def events
+  Event.where(organiser_id: id)
+ end 
+
 end
